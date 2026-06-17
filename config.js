2966c15cb70d8e50d85d2411ce288a9394f0d898
alessandrapalomino-eng <alessandra.palomino@upch.pe>
2026-06-16 21:50:07 -0500

    llave anonima real agregada

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://fgpioaxohruhbbxqwghu.supabase.co"; // Tu URL real
+const SUPABASE_ANON_KEY = "sb_publishable_eYPKuYraqhb2SrV0dvNNeA_VBdQobsK";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
